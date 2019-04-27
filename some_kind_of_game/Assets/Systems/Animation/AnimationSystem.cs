9aefc0692f82657f6ec84fda8184b31425743d5a
Michael Scherbakow <ryupold@gmail.com>
2019-04-27 17:55:05 +0200

    modified folder structure + added green fish

---
+++ some_kind_of_game/Assets/Systems/Animation/AnimationSystem.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using SystemBase;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+using UniRx.Operators;
+
+namespace Systems.Animation
+{
+    [GameSystem]
+    public class AnimationSystem : GameSystem<FinAnimationComponent>
+    {
+
+        public override void Register(FinAnimationComponent component)
+        {
+            component.FixedUpdateAsObservable()
+            .Select(_ => Time.deltaTime)
+            .Subscribe(delta =>
+            {
+                var angleDelta = ((int)component.Direction * delta * component.WobbleSpeed);
+
+                if (component.Direction == FinDirection.Left)
+                {
+                    if (component.CurrentAngle + angleDelta < -component.SpreadAngle / 2)
+                        component.Direction = FinDirection.Right;
+
+                    component.CurrentAngle = Math.Max(angleDelta + component.CurrentAngle, -component.SpreadAngle / 2);
+                }
+
+                if (component.Direction == FinDirection.Right)
+                {
+                    if (component.CurrentAngle + angleDelta >= component.SpreadAngle / 2)
+                    {
+                        angleDelta = component.CurrentAngle + angleDelta - component.SpreadAngle / 2;
+                        component.Direction = FinDirection.Left;
+                    }
+
+                    component.CurrentAngle = Math.Min(angleDelta + component.CurrentAngle, component.SpreadAngle / 2);
+                }
+
+                component.transform.Rotate(Vector3.up, angleDelta);
+            })
+            .AddTo(component);
+        }
+    }
+}
