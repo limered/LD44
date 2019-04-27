fbb324c5cb6afa291277c7a57c834a513c9e4253
Emil Wasilewski <emil.wasilewski@brickmakers.de>
2019-04-28 00:14:38 +0200

    add TailfinSystem and RotorComponent

---
+++ some_kind_of_game/Assets/Systems/PlayerUpgrades/TailFin/RotorComponent.cs
@@ -0,0 +1,10 @@
+﻿using SystemBase;
+
+namespace Systems.PlayerUpgrades.TailFin
+{
+    public class RotorComponent : GameComponent
+    {
+        public float AccelerationSummand = 80;
+        public float MaxSpeedSummand = 20;
+    }
+}
---
+++ some_kind_of_game/Assets/Systems/PlayerUpgrades/TailFin/TailFinSystem.cs
@@ -0,0 +1,39 @@
+﻿using SystemBase;
+using Systems.Control;
+using Systems.Movement;
+using UniRx;
+using UnityEngine;
+using Utils.Plugins;
+
+namespace Systems.PlayerUpgrades.TailFin
+{
+    [GameSystem(typeof(PlayerControlSystem))]
+    public class TailFinSystem : GameSystem<PlayerComponent, RotorComponent>
+    {
+        private readonly ReactiveProperty<PlayerComponent> _player = new ReactiveProperty<PlayerComponent>(null);
+        private RotorComponent _rotor;
+
+        public override void Register(RotorComponent component)
+        {
+            _rotor = component;
+
+            _player.WhereNotNull()
+                .Select(playerComponent => playerComponent.GetComponent<FishyMovementComponent>())
+                .Subscribe(movement =>
+                    {
+                        movement.AccelerationFactorModifier
+                            .Add(old => new Vector2(old.x + _rotor.AccelerationSummand, old.y));
+
+                        movement.MaxSpeedModifier
+                            .Add(old => new Vector2(old.x + _rotor.MaxSpeedSummand, old.y));
+                    }
+                )
+                .AddTo(component);
+        }
+
+        public override void Register(PlayerComponent component)
+        {
+            _player.Value = component;
+        }
+    }
+}
