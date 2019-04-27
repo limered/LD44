aa53b75cfac37093a758dc5b2842d537385f4de7
Sven Milker <sven.milker@brickmakers.de>
2019-04-27 18:11:29 +0200

    added menu buttons
    using TextMesh Pro

---
+++ some_kind_of_game/Assets/Design/Menu/QuitOnClick.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Design.Menu
+{
+    public class QuitOnClick : MonoBehaviour
+    {
+        public void Quit()
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+
+#else
+            Application.Quit();
+#endif
+        }
+    }
+}
---
+++ some_kind_of_game/Assets/Design/Menu/StartOnClick.cs
@@ -0,0 +1,14 @@
+﻿using GameState.States;
+using UniRx;
+using UnityEngine;
+
+namespace Design.Menu
+{
+    public class StartOnClick : MonoBehaviour
+    {
+        public void StartGame()
+        {
+            MessageBroker.Default.Publish(new Running());
+        }
+    }
+}
