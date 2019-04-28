09ab81246c9d338c4600f1b3f40bdf71b3cd895a
Emil Wasilewski <emil.wasilewski@brickmakers.de>
2019-04-28 20:57:45 +0200

    add room parts

---
+++ some_kind_of_game/Assets/Systems/Level/RoomComponent.cs
@@ -0,0 +1,13 @@
+﻿using SystemBase;
+using Systems.Control;
+using UniRx;
+using UnityEngine;
+
+namespace Systems.Level
+{
+    public class RoomComponent : GameComponent
+    {
+        public Collider2D ActivateCollider;
+        public ReactiveCommand OnEnteredRoom = new ReactiveCommand();
+    }
+}
