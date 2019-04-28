f534f40d15731b17b18b17080962a3c3a6dac5ad
Emil Wasilewski <emil.wasilewski@brickmakers.de>
2019-04-28 19:39:21 +0200

    add level spawner

---
+++ some_kind_of_game/Assets/Systems/Level/LevelGeneratorSystem.cs
@@ -0,0 +1,21 @@
+﻿using SystemBase;
+using UnityEngine;
+
+namespace Systems.Level
+{
+    [GameSystem]
+    public class LevelGeneratorSystem : GameSystem<LevelSpawner>
+    {
+        private int _partlength = 64;
+
+        public override void Register(LevelSpawner component)
+        {
+            for (var i = 0; i < component.PartsPerLevel; i++)
+            {
+                var rnd = Random.value * component.RoomPartPrefabs.Count;
+                Object.Instantiate(component.RoomPartPrefabs[(int) rnd], new Vector3(i* _partlength, 0, 0), Quaternion.identity,
+                    component.transform);
+            }
+        }
+    }
+}
---
+++ some_kind_of_game/Assets/Systems/Level/LevelSpawner.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using SystemBase;
+using UnityEngine;
+
+namespace Systems.Level
+{
+    public class LevelSpawner : GameComponent
+    {
+        public int PartsPerLevel = 5;
+        public List<GameObject> RoomPartPrefabs = new List<GameObject>();
+    }
+}
