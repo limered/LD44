931f7491bf26aca392a6fa1b29e9c6a29b25bae6
Emil Wasilewski <emil.wasilewski@brickmakers.de>
2019-04-28 23:13:10 +0200

    add level switch

--- some_kind_of_game/Assets/Systems/Level/LevelGeneratorSystem.cs
+++ some_kind_of_game/Assets/Systems/Level/LevelGeneratorSystem.cs
@@ -1,10 +1,14 @@
 ﻿using SystemBase;
+using Systems.Control;
+using UniRx;
+using UniRx.Triggers;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Systems.Level
 {
     [GameSystem]
-    public class LevelGeneratorSystem : GameSystem<LevelSpawner, PlayerSpawnComponent>
+    public class LevelGeneratorSystem : GameSystem<LevelSpawner, PlayerSpawnComponent, FinishComponent>
     {
         private int _partlength = 64;
         private int _spawnWidth = 12;
@@ -16,7 +20,7 @@ namespace Systems.Level
             for (var i = 0; i < component.PartsPerLevel; i++)
             {
                 var rnd = Random.value * component.RoomPartPrefabs.Count;
-                Object.Instantiate(component.RoomPartPrefabs[(int) rnd], new Vector3(i* _partlength, 0, 0), Quaternion.identity,
+                Object.Instantiate(component.RoomPartPrefabs[(int)rnd], new Vector3(i * _partlength, 0, 0), Quaternion.identity,
                     component.transform);
             }
             Object.Instantiate(component.FinishPrefab, new Vector3(component.PartsPerLevel * _partlength - 10, 0, 0), Quaternion.identity,
@@ -26,6 +30,14 @@ namespace Systems.Level
         public override void Register(PlayerSpawnComponent component)
         {
             Object.Instantiate(component.PlayerPrefab, component.transform.position, component.transform.rotation);
+        }
+
+        public override void Register(FinishComponent component)
+        {
+            component.ExitTrigger.OnTriggerEnter2DAsObservable()
+                .Where(d => d.attachedRigidbody.GetComponent<PlayerComponent>())
+                .Subscribe(d => SceneManager.LoadScene("Level"))
+                .AddTo(component);
         }
     }
 }
