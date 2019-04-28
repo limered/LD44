505dd2e63a47ac00935c42fa9e277a2dd739fbdb
Michael Scherbakow <ryupold@gmail.com>
2019-04-28 20:30:45 +0200

    [Kailax] reading Upgrade Slot configuration from UpgradeConfigComponent

--- some_kind_of_game/Assets/Systems/Player/SlotComponent.cs
+++ some_kind_of_game/Assets/Systems/Player/SlotComponent.cs
@@ -7,7 +7,6 @@ using UnityEngine;
 
 namespace Systems.Player
 {
-    [ExecuteAlways]
     public class SlotComponent : GameComponent
     {
         public string Name = "Enter Name";
@@ -16,24 +15,6 @@ namespace Systems.Player
         [Space(30)]
         public StringReactiveProperty SelectedItem = new StringReactiveProperty("");
 
-        new void Start()
-        {
-            if (Application.IsPlaying(gameObject))
-            {
-                base.Start();
-            }
-            else // Editor logic
-            {
-                var player = GameObject.FindObjectOfType(typeof(PlayerComponent)) as PlayerComponent;
-                if (!player) throw new Exception("no PlayerComponent found in Scene");
-
-                var slot = player.Slots.Any(x => x.Component == this)
-                    ? player.Slots.Find(x => x.Component == this)
-                    : new Slot { Component = this };
-                player.Slots.Remove(slot);
-                player.Slots.Add(slot);
-            }
-        }
     }
 
     [Serializable]
