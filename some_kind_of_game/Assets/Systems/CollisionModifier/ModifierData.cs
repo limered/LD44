578c9fb90fbfae29a0a3787fe327c387bd2b201a
Emil Wasilewski <me@limered.de>
2019-04-29 15:37:28 +0200

    add modifier collisions, algae group fo slowdown

---
+++ some_kind_of_game/Assets/Systems/CollisionModifier/ModifierData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+namespace Systems.CollisionModifier
+{
+    [Serializable]
+    public class ModifierData
+    {
+        public ModifierType Type;
+        public float SummandValue;
+        public Vector2 SummandVector;
+        public bool isTimed;
+        public float Duration;
+    }
+}
