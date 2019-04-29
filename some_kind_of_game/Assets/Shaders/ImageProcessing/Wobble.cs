8f55df171d7bc6a9cc68b17ab617d6375262c988
Emil Wasilewski <emil.wasilewski@brickmakers.de>
2019-04-29 23:47:08 +0200

    add wobble shader

---
+++ some_kind_of_game/Assets/Shaders/ImageProcessing/Wobble.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Shaders.ImageProcessing
+{
+    public class Wobble : MonoBehaviour
+    {
+        public Material RenderMaterial;
+
+        private RenderTexture _renderTexture;
+
+        private void OnEnable()
+        {
+            var camera = GetComponent<Camera>();
+            const int height = 540;
+            const int width = 960;
+            _renderTexture = new RenderTexture(width, height, 16) { filterMode = FilterMode.Point };
+        }
+
+        private void OnDisable()
+        {
+            Destroy(_renderTexture);
+        }
+
+        private void OnRenderImage(RenderTexture src, RenderTexture dest)
+        {
+            Graphics.Blit(src, dest, RenderMaterial);
+        }
+    }
+}
