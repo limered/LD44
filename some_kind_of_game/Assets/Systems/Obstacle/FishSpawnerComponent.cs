ecec7587f9e0412104f8f27bcc4a29dc5dc71f02
Michael Scherbakow <ryupold@gmail.com>
2019-04-29 22:53:44 +0200

    [Spawner] added BlueSwarmSpawner

--- some_kind_of_game/Assets/Systems/Obstacle/FishSpawnerComponent.cs
+++ some_kind_of_game/Assets/Systems/Obstacle/FishSpawnerComponent.cs
@@ -7,6 +7,12 @@ namespace Systems.Obstacle
     public class FishSpawnerComponent : GameComponent
     {
         public GameObject[] SpawnPrefabs;
+
+        public uint Amount = 1;
+
+        [Range(0, 100)]
+        public float Radius = 10f;
+
         public RoomComponent Room;
         public Collider2D TriggerOverwrite;
     }
